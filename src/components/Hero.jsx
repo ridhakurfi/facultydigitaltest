6f28af71100aef3f06193eed244abd49e156e1b9
ridhakurfi <ridhakurfi@gmail.com>
2026-05-06 09:53:13 +0700

    6

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ function Hero() {
       image:
         hero3,
       title: 'Escape to the city',
-      subtitle: 'SAVETHEQUEENY Collection'
+      subtitle: 'SAVETHEQUEEN Collection'
     }
   ]
 
