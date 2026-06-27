effef0d88c4819910d9e1f9be9651520fae04a17
ridhakurfi <ridhakurfi@gmail.com>
2026-06-27 20:56:49 +0700

    27

--- src/components/GlobeTrot.jsx
+++ src/components/GlobeTrot.jsx
@@ -7,7 +7,7 @@ function GlobeTrot() {
     <section className="globe-trot">
 
       <div className="globe-left">
-        <h2>Globe-trot with Pan Atlantic</h2>
+        <h2>Globe-trot with Pan Atlantis</h2>
 
         <ul className="globe-menu">
           <li>All</li>
