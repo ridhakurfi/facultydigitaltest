10ec04350052f808d30e24ff25d74e9a6e7b34f2
ridhakurfi <ridhakurfi@gmail.com>
2026-07-10 14:26:34 +0700

    5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ function Hero() {
       <div className="hero-overlay"></div>
 
       <div className="hero-content">
-        <p className="hero-tag">February Spotlight</p>
+        <p className="hero-tag">March Spotlight</p>
 
         <h1>{slides[currentSlide].title}</h1>
 
