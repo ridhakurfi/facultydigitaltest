247cae1f6bb00c9e00a8bcb06e37db74018483cc
ridhakurfi <ridhakurfi@gmail.com>
2026-08-12 18:51:07 +0700

    add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ function Hero() {
       <div className="hero-overlay"></div>
 
       <div className="hero-content">
-        <p className="hero-tag">Fated Spotlight</p>
+        <p className="hero-tag">Solar Spotlight</p>
 
         <h1>{slides[currentSlide].title}</h1>
 
